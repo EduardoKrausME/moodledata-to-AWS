0b81cfe95c52d2a43dd9afaabb0b837890b86490
Eduardo Kraus <kraus@eduardokraus.com>
2016-10-28 17:42:04 -0200

    _load.php missing

---
+++ _s3/_load.php
@@ -0,0 +1,18 @@
+<?php
+/***************************
+ * User: Eduardo Kraus
+ * @url https://www.eduardokraus.com/
+ * Date: 01/01/2016
+ * Time: 22:50
+ ***************************/
+
+error_reporting ( 0 );
+ini_set ( 'display_errors', 'Off' );
+
+if ( !isset( $aws_s3_mapping ) ) {
+    require_once __DIR__ . 'S3Filedir.php';
+    require_once __DIR__ . '/aws/aws-autoloader.php';
+}
+
+$file = new S3Filedir();
+$file->isFileInAmazon ( get_file_argument () );
